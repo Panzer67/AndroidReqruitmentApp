6c12848065b154d39165623cf687d08329216086
Sinan Ozdil <sinan.ozdil@ordina.nl>
2015-05-18 16:02:50 +0200

    Blanco AndroidProject

---
+++ app/src/main/java/com/example/soz20659/reqruitmentapp/MyRenderer.java
@@ -0,0 +1,26 @@
+package com.example.soz20659.reqruitmentapp;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by soz20659 on 7-5-2015.
+ */
+public class MyRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
